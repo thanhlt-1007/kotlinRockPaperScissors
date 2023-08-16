90dc03d1c5a24e3342324b37e38a2084ada088d9
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2023-08-16 11:44:38 +0700

    Add the getUserChoice function to Game.kt

--- src/main.kt
+++ src/main.kt
@@ -1,7 +1,7 @@
 fun main() {
   val options = arrayOf("Rock", "Paper", "Scissors")
   val gameChoice = getGameChoice(options)
-  getUserChoice(options)
+  val userChoice = getUserChoice(options)
 }
 
 fun getGameChoice(optionsParam: Array<String>) : String {
@@ -9,14 +9,33 @@ fun getGameChoice(optionsParam: Array<String>) : String {
   return optionsParam[index]
 }
 
-fun getUserChoice(optionsParam: Array<String>) {
-  // Ask the user for their choice
-  println("Please enter one of the following:")
-  for (item in optionsParam) {
-    println("$item")
-  }
-  println(".")
+fun getUserChoice(optionsParam: Array<String>) : String {
+  // We'll use the isValidChoice variable to indicate whether the user has entered a valid choice.
+  var isValidChoice = false
+  var userChoice = ""
 
-  // Read the user input
-  val userInput = readLine()
+  // Loop until the user enters a valid choice
+  // Keep looping until isValidChoice is true
+  while (!isValidChoice) {
+    // Ask the user for their choice
+    print("Please enter one of the following:")
+    for (item in optionsParam) {
+      print(" $item")
+    }
+    println(".")
+
+    // Read the user input
+    val userInput = readLine()
+
+    // validate the user input
+    if (userInput != null && userInput in optionsParam) {
+      isValidChoice = true
+      userChoice = userInput
+    }
+
+    // If the choice is invalid, inform the user
+    if (!isValidChoice) println("You must enter a valid choice.")
+  }
+
+  return userChoice
 }
